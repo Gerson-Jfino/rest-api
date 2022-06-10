09ca803fb851f2151f96a42c4d347819a3ab8831
Gerson Maoze <gerson.maoze@marrar.co.mz>
2022-06-10 19:30:54 +0200

    configure cors

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({})
+    }
+
+    next();
+})
 
 app.use('/products', productsRoutes);
 app.use('/orders', pedidosRoutes);
@@ -17,14 +31,14 @@ app.use((req, res, next) => {
     const error = new Error("undefiened route");
     error.status = 404;
     next(error);
-})
+});
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
+    res.status(error.status || 500);
     return res.send({
         message: error.message
-    })
-})
+    });
+});
 
 
 module.exports = app;
