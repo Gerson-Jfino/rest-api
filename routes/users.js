19d6c0de3f43b92c5006a7ce4b20d2bbf1df9654
Gerson Maoze <gerson.maoze@marrar.co.mz>
2022-06-27 19:30:38 +0200

    Authenticate users using token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const mysql = require('../services/mysql').pool
 const router = express.Router()
 
@@ -20,8 +21,8 @@ router.post('/register', (req, res, next) => {
                         conn.query('INSERT INTO users (name, email, password) VALUES (?,?,?)',
                             [req.body.name, req.body.email, hash], 
                             (err, result, fields) => {
-                                if(err) {return res.status(500).send({err})}
                                 conn.release()
+                                if(err) {return res.status(500).send({err})}
                                 const {name, email} = req.body
                                 const response = {
                                     message: 'user created successfully',
@@ -36,9 +37,39 @@ router.post('/register', (req, res, next) => {
                     })
                 }
             })
+    })
+})
 
-        
-        
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error) {return res.status(500).send({error})}
+        conn.query('SELECT * FROM users WHERE email = ?;',
+            [req.body.email], (err, result, field) => {
+                conn.release()
+                if(err) { return res.status(500).send({err})}
+                if(result.length === 0) {
+                    return res.status(401).send({
+                        message: 'Unauthorized'
+                    })
+                }
+                const {password} = result[0]
+                bcrypt.compare(req.body.password, password, (err, value) => {
+                    if (err) {return res.status(401).send({message: 'Unauthorized'})}
+                    if(value) {
+                        const {name, email} = result[0]
+                        const user = {name, email}
+                        const token = jwt.sign(user, "any,SecreteKey", {
+                            expiresIn: "1h"
+                        })
+                        const response = {
+                            message: 'Authenticated successfully',
+                            token
+                        }
+                        return res.status(200).send(response)
+                    }
+                    return res.status(401).send({message: 'Unauthorized'})
+                })
+            })
     })
 })
 
