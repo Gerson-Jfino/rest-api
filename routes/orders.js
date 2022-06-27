e01f35fb6d8b7c3311e6e6252e5a9934cd4b4c00
Gerson Maoze <gerson.maoze@marrar.co.mz>
2022-06-27 20:43:37 +0200

    Routes authentication

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const ordersController = require('../controllers/orders-controller')
+const auth = require("../middleware/login")
 const router = express.Router()
 
-router.get('/', ordersController.index)
-router.get('/:order_id', ordersController.show)
-router.post('/', ordersController.store)
-router.delete('/:order_id', ordersController.destroy)
+router.get('/', auth,ordersController.index)
+router.get('/:order_id', auth,ordersController.show)
+router.post('/', auth, ordersController.store)
+router.delete('/:order_id', auth, ordersController.destroy)
 
 module.exports = router
