3aaf7d7cda133bbd2d3cece60bec0ae062bc80fb
Gerson Maoze <gerson.maoze@marrar.co.mz>
2022-06-30 19:39:37 +0200

    refactoring user controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,73 +1,65 @@
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const mysql = require('../services/mysql').pool
+const jwt = require('jsonwebtoken');
+const { token } = require('morgan');
+const mysql = require('../services/mysql')
 
 module.exports = {
-    register: (req, res, next) => {
-        mysql.getConnection((error, conn) => {
-            if(error) {return res.status(500).send(error)}
-            conn.query('SELECT * FROM users where email = ?;',
-                [req.body.email], (err, result) => {
-                    if(err) {return res.status(500).send({err})}
-                    if(result.length !== 0){
-                        conn.release()
-                        return res.status(409).send({
-                            message: "user already exist"
-                        })
-                    } else {
-                        bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-                            if(errBcrypt) {return res.status(500).send({ errBcrypt })}
-                            conn.query('INSERT INTO users (name, email, password) VALUES (?,?,?)',
-                                [req.body.name, req.body.email, hash], 
-                                (err, result, fields) => {
-                                    conn.release()
-                                    if(err) {return res.status(500).send({err})}
-                                    const {name, email} = req.body
-                                    const response = {
-                                        message: 'user created successfully',
-                                        user: {
-                                            id: result.inserId,
-                                            name,
-                                            email
-                                        }
-                                    }
-                                    return res.status(200).send(response)
-                                })
-                        })
-                    }
-                })
-        })
-    },
-    login: (req, res, next) => {
-        mysql.getConnection((error, conn) => {
-            if(error) {return res.status(500).send({error})}
-            conn.query('SELECT * FROM users WHERE email = ?;',
-                [req.body.email], (err, result, field) => {
-                    conn.release()
-                    if(err) { return res.status(500).send({err})}
-                    if(result.length === 0) {
-                        return res.status(401).send({
-                            message: 'Unauthorized'
-                        })
-                    }
-                    const {password} = result[0]
-                    bcrypt.compare(req.body.password, password, (err, value) => {
-                        if (err) {return res.status(401).send({message: 'Unauthorized'})}
-                        if(value) {
-                            const {name, email} = result[0]
-                            const user = {name, email}
-                            const token = jwt.sign(user, "any,SecreteKey", {
-                                expiresIn: "1h"
-                            })
-                            const response = {
-                                message: 'Authenticated successfully',
-                                token
-                            }
-                            return res.status(200).send(response)
+    register: async (req, res, next) => {
+        try {
+            const result = await mysql.execute('SELECT * FROM users WHERE email = ?;', [req.body.email]);
+            if(result.length !== 0) {
+                res.status(409).send({
+                    message: 'This email has been taken.'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, async (error, hashPassword) => {
+                    if(error){return res.status(500).send(error);}
+                    const result = await  mysql.execute('INSERT INTO users (name, email, password) VALUES (?,?,?);', [req.body.name, req.body.email, hashPassword]);
+                    const {name, email} = req.body;
+                    const response = {
+                        message: "Account created successfully.",
+                        user: {
+                            id: result.insertId,
+                            name,
+                            email
                         }
-                        return res.status(401).send({message: 'Unauthorized'})
-                    })
+                    };
+                    return res.status(200).send(response);
                 })
-        })
+            }    
+        } catch (error) {
+            return res.status(500).send(error)
+        }
+    },
+    login: async (req, res, next)  => { 
+        try {
+            const user = await mysql.execute('SELECT * FROM users WHERE email = ?;', [req.body.email]);
+            if( user.length === 0 ) {
+                return res.status(401).send({
+                    message: 'Unauthorized'
+                });
+            }
+            const { password } = user[0];
+            bcrypt.compare(req.body.password, password, async (err, value) => {
+                if (err) { return res.status(401).send({message: 'Unauthorized'}); }
+                if (!value) { return res.status(401).send({message: 'Unauthorized'}); }
+                if (value) {
+                    function userData () {
+                        const {id, name, email} = user[0];
+                        return { id, name, email };
+                    };
+                    const token = await jwt.sign(userData(), 'any,SecreteKey', {
+                        expiresIn: "1h"
+                    });
+                    const response = {
+                        message: 'Authenticated successfully',
+                        token
+                    }
+                    res.status(200).send(response);
+                }
+            })
+        } catch (error) {
+            return res.status(500).send(error)
+        }
     }
 }
